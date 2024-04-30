01dd499cf190d65185902c4f673c83cec351c3f4
marcielssilva <marciel.ssilva2@gmail.com>
2024-04-30 05:48:20 -0300

    UPDATE ESCOPO COMENTARIOS E PALAVRAS RESERVADAS
    ALGORITMOS PARA CONSULTA NAO EXECUCAO

---
+++ projeto-calculadora/src/Calculadora.java
@@ -0,0 +1,40 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author  Marciel S. Silva
+ * @version 1.0
+ * @since   30/04/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+    /*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+ * observe na dificuldade de leitura pela nomenclatura de variaveis no metodo abaixo
+ * /
+public int somaMultiplica (int n, int x, String m){
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x
+    }
+    return r;
+}
+*/
+}
