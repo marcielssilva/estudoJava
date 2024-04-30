2a7d369c732d9f33bf06d3755d860c142366d0b0
marcielssilva <marciel.ssilva2@gmail.com>
2024-04-30 16:23:46 -0300

    CONTROLE DE FLUXO SIMPLES +UPDATE CONTROLE DE FLUXO COMPOSTO

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,11 +1,15 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 27.0;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo Saldo: " + saldo);
+        } else {
+            System.out.println("Saldo Insuficiente");
 
-        System.out.println(saldo);
+            System.out.println(saldo);
+        }
     }
 }
